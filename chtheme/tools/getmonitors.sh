cd63def29c88e19c4f4c9de0b9037c5a41c13979
B.Thompson <bernardsthompson@gmail.com>
2023-04-28 21:54:53 -0700

    update

--- chtheme/tools/getmonitors.sh
+++ chtheme/tools/getmonitors.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+montmp=$PWD/mon.dat
 echo "Monitor Name: [Background] "
-xfconf-query -c xfce4-desktop -l /backdrop/screen0/ | cut -d/ -f4 | sort -u | grep ^monitor > /tmp/mon.txt
+xfconf-query -c xfce4-desktop -l /backdrop/screen0/ | cut -d/ -f4 | sort -u | grep ^monitor > "$montmp" 
 while read file; do
 echo -n "$file: "; xfconf-query -c xfce4-desktop -p /backdrop/screen0/"$file"/workspace0/last-image | rev | cut -d\/ -f1 | rev
-done < /tmp/mon.txt 
+done < "$montmp" 
 echo "Connected: "
 xrandr | awk '{ print "monitor"$1":",$2 }' | grep connected | grep -v disconnected
-
+rm "$montmp"
