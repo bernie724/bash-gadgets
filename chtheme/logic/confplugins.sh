d12cbcc2895bc9b8713c9cf7e48c40089c28aa29
B.Thompson <bernardsthompson@gmail.com>
2023-04-30 12:17:09 -0700

    update

--- chtheme/logic/confplugins.sh
+++ chtheme/logic/confplugins.sh
@@ -7,6 +7,7 @@ xfconf-query -c xfce4-panel -p /plugins/plugin-$pager -n -t 'string' -s "pager"
 xfconf-query -c xfce4-panel -p /plugins/plugin-$tlist -n -t 'string' -s "tasklist"
 xfconf-query -c xfce4-panel -p /plugins/plugin-$space -n -t 'string' -s "separator"
 xfconf-query -c xfce4-panel -p /plugins/plugin-$stray -n -t 'string' -s "systray"
+xfconf-query -c xfce4-panel -p /plugins/plugin-$lsetup -n -t 'string' -s "launcher"
 xfconf-query -c xfce4-panel -p /plugins/plugin-$sshot -n -t 'string' -s "screenshooter"
 xfconf-query -c xfce4-panel -p /plugins/plugin-$audio -n -t 'string' -s "pulseaudio"
 xfconf-query -c xfce4-panel -p /plugins/plugin-$power -n -t 'string' -s "power-manager-plugin"
@@ -15,7 +16,9 @@ xfconf-query -c xfce4-panel -p /plugins/plugin-$launch -n -t 'string' -s "launch
 xfconf-query -c xfce4-panel -p /plugins/plugin-$action -n -t 'string' -s "actions"
 
 ##create launcher array
-xfconf-query -c xfce4-panel -p /plugins/plugin-$launch/items -n -a -t string -s "xfce4-terminal-emulator.desktop" -t string -s "xfce4-web-browser.desktop" -t string -s "xfce4-file-manager.desktop" #-t string -s "xfce4-settings-manager.desktop" 
+xfconf-query -c xfce4-panel -p /plugins/plugin-$launch/items -n -a -t string -s "xfce4-terminal-emulator.desktop" -t string -s "xfce4-file-manager.desktop" #-t string -s "xfce4-settings-manager.desktop" 
+xfconf-query -c xfce4-panel -p /plugins/plugin-$lsetup/items -n -a -t string -s "xfce4-web-browser.desktop" 
+
 ##create actions icons
 xfconf-query -c xfce4-panel -p /plugins/plugin-$action/items -n -a -t string -s "+lock-screen" -t string -s "-switch-user" -t string -s "-separator" -t string -s "-suspend" -t string -s "-hibernate" -t string -s "-hybrid-sleep" -t string -s "-separator" -t string -s "-shutdown" -t string -s "-restart" -t string -s "-separator" -t string -s "+logout" -t string -s "-logout-dialog"
 ##create tasklist properties
@@ -23,6 +26,7 @@ xfconf-query -c xfce4-panel -p /plugins/plugin-$tlist/middle-click -n -t 'int' -
 xfconf-query -c xfce4-panel -p /plugins/plugin-$tlist/grouping -n -t 'bool' -s 'false' 
 xfconf-query -c xfce4-panel -p /plugins/plugin-$tlist/show-tooltip -n -t 'bool' -s 'false' 
 xfconf-query -c xfce4-panel -p /plugins/plugin-$tlist/show-labels -n -t 'bool' -s "$psl1" 
+xfconf-query -c xfce4-panel -p /plugins/plugin-$tlist/sort-order -n -t 'int' -s "0" 
 xfconf-query -c xfce4-panel -p /plugins/plugin-$space/expand -n -t 'bool' -s "true"
 xfconf-query -c xfce4-panel -p /plugins/plugin-$space/style -n -t 'int' -s "0"
 ##workspace changer properties
@@ -30,7 +34,7 @@ xfconf-query -c xfce4-panel -p /plugins/plugin-$pager/rows -n -t 'int' -s "1"
 xfconf-query -c xfce4-panel -p /plugins/plugin-$pager/miniature-view -n -t 'bool' -s "false" 
 ##clock setup
 xfconf-query -c xfce4-panel -p /plugins/plugin-$clock/mode -n -t 'int' -s "0" 
-xfconf-query -c xfce4-panel -p /plugins/plugin-$clock/tooltip-format -n -t string -s "%T"
+xfconf-query -c xfce4-panel -p /plugins/plugin-$clock/tooltip-format -n -t string -s "%h %d %I:%M:%S" #"%T"
 ##menu properties
 xfconf-query -c xfce4-panel -p /plugins/plugin-$menu/button-icon -n -t 'string' -s "$menuicon" 
 xfconf-query -c xfce4-panel -p /plugins/plugin-$menu/button-title -n -t 'string' -s "$menuname" 
--- chtheme/logic/confstatic.sh
+++ chtheme/logic/confstatic.sh
@@ -18,7 +18,7 @@ xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/show-panel-label -n 
 
 ##create arrays  
 #default mainpanel plugin positions
-xfconf-query -c xfce4-panel -p /panels/panel-$mainpanel/plugin-ids -n -a -t int -s $menu -t int -s $clock -t int -s $launch -t int -s $pager -t int -s $tlist -t int -s $space -t int -s $stray -t int -s $sshot -t int -s $audio -t int -s $power -t int -s $notify -t int -s $action
+xfconf-query -c xfce4-panel -p /panels/panel-$mainpanel/plugin-ids -n -a -t int -s $menu -t int -s $clock -t int -s $launch -t int -s $pager -t int -s $tlist -t int -s $space -t int -s $stray -t int -s $lsetup -t int -s $sshot -t int -s $audio -t int -s $power -t int -s $notify -t int -s $action
 #related to panel color
 xfconf-query -c xfce4-panel -p /panels/panel-$mainpanel/background-color -n -a -t int -s 0 -t int -s 65535 -t int -s 0 -t int -s 65535
 xfconf-query -c xfce4-panel -p /panels/panel-$mainpanel/background-rgba -n -a -t int -s 1 -t int -s 1 -t int -s 1 -t int -s 0
